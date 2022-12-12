bd58a1028b41d4c3f8d11bd23e5e2625c6969458
poojarathore13 <rathore.pooja1305@gmail.com>
2022-12-12 23:29:14 +0530

    added link to home page in 404!

--- pages/404.js
+++ pages/404.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import ScrollProgress from '../components/ScrollProgress'
 import Head from 'next/head'
 import AOS from 'aos';
+import Link from 'next/link';
 import 'aos/dist/aos.css';
 
 const aboutus = () => {
@@ -15,7 +16,7 @@ const aboutus = () => {
     </Head>
     <div className='w-4/5 mx-auto p-8 my-4'>
     <img  src="static/404.svg" className="w-3/4 mx-auto"/>
-     <div><a >Click Here</a> to Go to Home Page</div>
+     <div><Link href="/" >Click Here</Link> to Go to Home Page</div>
       </div>
     </>
   )
