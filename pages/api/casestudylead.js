d6071bdcf00aea6a10bbf02bf5178d5fffbb83cf
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2022-12-18 13:58:57 +0530

    update variables

--- pages/api/casestudylead.js
+++ pages/api/casestudylead.js
@@ -16,13 +16,13 @@ const handler = async (req, res) => {
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-              user: process.env.sending_email,
-              pass: process.env.app_password
+              user: "anshul.d@analyticsliv.com",
+              pass: "cmptwmtqjrgdhgqn"
             }
           });
           
           var mailOptions = {
-            from: process.env.sending_email,
+            from: "anshul.d@analyticsliv.com",
             to: req.body.email,
             subject: 'AnalyticsLiv - Download link for the case study',
             html: `<html><head><style>#container{position:relative;width:100%;height:300px;display:flex;justify-content:center}#bod{position:relative;width:500px;height:300px;text-align:center}</style></head><body><div id="container"><div id="bod"><img style="width:300;height:50;margin-top:30px;margin-bottom:10px" src="https://storage.googleapis.com/website-bucket-uploads/logo.png"><div id="title"><h2>Thankyou for showing interest on our Case Study!!</h2></div><div id="content"><h3>To download the Case Study on ${req.body.casestudy}, Please click <a href="http://sdssdsd">here.</a></h3></div></div></div></body></html>`
