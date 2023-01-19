f0a3c5da7c9b6509a61007caa4b0990db134ab1e
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-01-19 10:06:16 +0530

    sitemap added

---
+++ pages/api/sitemap.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Blog from "../../models/Blog";
+import Casestudy from "../../models/Casestudy";
+import connectDb from "../../middleware/mongoose";
+
+
+const handler = async (req, res) => {
+    var sitemap = [];
+    let blog = await Blog.find({active:true}, {slug:1});
+    let casestudy = await Casestudy.find({active:true}, { slug:1});
+    
+    for(var i=0; i<blog.length; i++){
+        sitemap.push({'url':'https://analyticsliv.com/blogs/'+blog[i].slug});
+    }
+    for(var i=0; i<casestudy.length; i++){
+        sitemap.push({'url':'https://analyticsliv.com/case-studies/'+casestudy[i].slug});
+    }
+
+    res.status(200).json({ sitemap });
+}
+
+
+export default connectDb(handler);
