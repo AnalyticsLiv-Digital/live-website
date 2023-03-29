61a65bdb08478608e29a20d262f6dac77f83a61e
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-03-29 19:05:34 +0530

    first commitsd

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -10,6 +10,7 @@ const handler = async (req, res) => {
     let casestudy = await Casestudy.find({active:true}, { slug:1});
     
     for(var i=0; i<blog.length; i++){
+        
         sitemap.push({'url':'https://analyticsliv.com/blogs/'+blog[i].slug});
     }
     for(var i=0; i<casestudy.length; i++){
