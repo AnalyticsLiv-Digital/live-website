3c82734049e36d9022a368e7dd104aa4aa78dbd8
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-10-16 14:03:04 +0530

    first commitsd

--- pages/api/ecommercecontact.js
+++ pages/api/ecommercecontact.js
@@ -28,7 +28,7 @@ const handler = async (req, res) => {
 
           var mailOptions1 = {
             from: "support@analyticsliv.com",
-            to: ["anshul.d@analyticsliv.com"],
+            to: ["anshul.d@analyticsliv.com","anuj@analyticsliv.com","nitya@analyticsliv.com","chirag.rathod@analyticsliv.com","esha.chhaparwal@analyticsliv.com"],
             subject: 'New Ecommerce Enquiry!!',
             html: `Enquiry Submitted by <br> Full Name - ${req.body.fullName}  <br> Email- ${req.body.email} <br> Contact - ${req.body.contact} <br> Message - ${req.body.message} <br> Website - ${req.body.website} `
           };
