59f80720470cc19aca34efe59d243dc7c6f4b1c7
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-10-19 21:18:04 +0530

    first commitsd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    swcMinify: true,
+    async headers() {
+        return [
+            {
+                // Sets security headers for all routes
+                source: '/(.*)',
+                headers: [
+                  {
+                    key: 'Strict-Transport-Security',
+                    value: 'max-age=31536000; includeSubDomains',
+                },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(self "https://analyticsliv.com"), microphone=()'
+                    }
+  
+                ],
+            },
+        ];
+    },
   }
   
   module.exports = nextConfig
