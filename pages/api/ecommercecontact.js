890ab1f373715061f46e837510d6e5a36f1ac190
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-11-23 14:52:45 +0530

    first commitsd

--- pages/api/ecommercecontact.js
+++ pages/api/ecommercecontact.js
@@ -28,7 +28,7 @@ const handler = async (req, res) => {
 
           var mailOptions1 = {
             from: "support@analyticsliv.com",
-            to: ["sales@analyticsliv.com","anshul.d@analyticsliv.com","anuj@analyticsliv.com","nitya@analyticsliv.com","chirag.rathod@analyticsliv.com","esha.chhaparwal@analyticsliv.com"],
+            to: ["ashwani.singh@analyticsliv.com","sales@analyticsliv.com","anshul.d@analyticsliv.com","anuj@analyticsliv.com","nitya@analyticsliv.com","chirag.rathod@analyticsliv.com","esha.chhaparwal@analyticsliv.com"],
             subject: 'New Ecommerce Enquiry!!',
             html: `Enquiry Submitted by <br> Full Name - ${req.body.fullName}  <br> Email- ${req.body.email} <br> Contact - ${req.body.contact} <br> Message - ${req.body.message} <br> Website - ${req.body.website} `
           };
--- pages/api/gtmcontact.js
+++ pages/api/gtmcontact.js
@@ -28,7 +28,7 @@ const handler = async (req, res) => {
 
           var mailOptions1 = {
             from: "support@analyticsliv.com",
-            to: ["sales@analyticsliv.com","anshul.d@analyticsliv.com","anuj@analyticsliv.com","nitya@analyticsliv.com","rajvi@analyticsliv.com","ansuya@analyticsliv.com"],
+            to: ["ashwani.singh@analyticsliv.com","sales@analyticsliv.com","anshul.d@analyticsliv.com","anuj@analyticsliv.com","nitya@analyticsliv.com","rajvi@analyticsliv.com","ansuya@analyticsliv.com"],
             subject: 'New GTM Enquiry!!',
             html: `Enquiry Submitted by <br> Full Name - ${req.body.fullName}  <br> Email- ${req.body.email} <br> Contact - ${req.body.contact} <br> Message - ${req.body.message} <br> Website - ${req.body.website} `
           };
