dd390c7044ceb5518238c9a67b63766970db604e
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2023-12-07 12:43:59 +0530

    first commitsd

--- pages/services/analytics.js
+++ pages/services/analytics.js
@@ -21,7 +21,37 @@ const index= () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2500
+        autoplaySpeed: 2500,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            }
+          ]
     };
     const { query } = useRouter();
 
