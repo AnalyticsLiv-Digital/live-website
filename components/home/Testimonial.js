22f55674b2c96137ed7ea083f95f6aca2901f06b
pratapvanol <148755485+pratapvanol@users.noreply.github.com>
2024-03-11 11:20:32 +0530

    Testimonial slide to show changes

--- components/home/Testimonial.js
+++ components/home/Testimonial.js
@@ -44,6 +44,14 @@ const Testimonial = () => {
             {
                 breakpoint: 988,
                 settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     initialSlide: 2,
