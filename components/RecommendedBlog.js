1ac99b26e6b87a058de8475f88d81a462cc43088
madkpi <madkpi@analyticsliv.com>
2024-07-16 14:09:27 +0530

    changes in recommended component

--- components/RecommendedBlog.js
+++ components/RecommendedBlog.js
@@ -1,26 +1,17 @@
-import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
 
 const RecommendedBlogs = ({ recommendedBlogsdata }) => {
-    const [recommendedBlogs, setRecommendedBlogs] = useState([]);
-
-    useEffect(() => {
-        if (recommendedBlogsdata) {
-            setRecommendedBlogs(recommendedBlogsdata);
-        }
-    }, [recommendedBlogsdata]);
-
 
     return (
         <section className='lg:flex lg:ml-[5.5%] lg:w-[67%] lg:justify-start mb-5 pb-5 max-lg:mx-7 space-y-2 lg:space-y-0 bg-white'>
 
             <div className="recommended-blogs mt-3 lg:p-5">
-                {recommendedBlogs?.length >= 2 && (
+                {recommendedBlogsdata?.length >= 2 && (
                     <div className="text-2xl md:text-3xl lg:text-4xl text-[#334155] font-semibold m-3 sm:pl-2">
                         Recommended Blogs
                     </div>
                 )}            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    {recommendedBlogs?.length >= 2 && recommendedBlogs?.slice(0, 2)?.map((blog) => (
+                    {recommendedBlogsdata?.length >= 2 && recommendedBlogsdata?.slice(0, 2)?.map((blog) => (
                         <Link key={blog.id} href={`/blogs/${blog.slug}`}>
                             <div className="text-primary mt-4 inline-block cursor-pointer">
                                 <div className="bg-white md:min-h-[45dvh] p-2 rounded-lg shadow-lg flex flex-col justify-between">
