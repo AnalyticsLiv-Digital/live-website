b0e30d47920c9273fec9a951426f6ff10f30e19a
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2024-09-26 10:32:14 +0530

    training page new banner added

--- pages/services/training.js
+++ pages/services/training.js
@@ -51,7 +51,7 @@ const training = () => {
             </div>
           </div>
           <div className='w-1/2 max-lg:hidden'>
-            <img src='/training-banner.jpg' className='w-full h-full' alt='gtm' />
+            <img src='/Training_Page-banner.png' className='w-full h-full' alt='gtm' />
           </div>
         </div>
       </section>
