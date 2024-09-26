964d224784e559ac0a6b6471698d1e46a06b203e
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2024-09-26 18:24:30 +0530

    Cookie webinar page initial setup

---
+++ pages/cookie-webinar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const cookieWebinar = () => {
+    return (
+        <div>
+            <div className='bg-[#2E2C37]  pt-5'>
+                <div className='flex justify-between'>
+                    <img src='/AnalyticsLiv Logo.png' alt='analyticsliv-logo' className='bg-white rounded-full px-4' />
+                    <img src='/Google_Marketing_Plateform_Certified_Logo 1.png' alt='google-partner' />
+                </div>
+                <div
+                    style={{
+                        backgroundImage: 'url(/vectormain.png)',
+                        backgroundSize: '',
+                        backgroundPosition: 'bottom right',
+                        backgroundRepeat: 'no-repeat',
+                    }} className='pl-10'
+                >
+                    <div>
+                        <div className='text-3xl font-black text-white'>Google Basic Consent vs. Advanced Consent-<br></br>Optimising Your Data Strategy</div>
+                    </div>
+                    <img src='/IMG.png' alt='analyticsliv-logo' className='right-0' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default cookieWebinar
