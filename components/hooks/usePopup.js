cfd51d1e0294063d8d6ceafec25ce4e3f3765f94
madkpi <madkpi@analyticsliv.com>
2024-10-16 10:35:26 +0530

    popup paused

--- components/hooks/usePopup.js
+++ components/hooks/usePopup.js
@@ -3,23 +3,23 @@ import { useState, useEffect } from 'react';
 const useWebinarPopup = () => {
     const [showPopup, setShowPopup] = useState(false);
 
-    useEffect(() => {
-        const hasClosed = localStorage.getItem('webinarPopupClosed');
-        const hasRegistered = localStorage.getItem('webinarRegistered');
+    // useEffect(() => {
+    //     const hasClosed = localStorage.getItem('webinarPopupClosed');
+    //     const hasRegistered = localStorage.getItem('webinarRegistered');
 
-        const currentTime = Date.now();
-        // Show popup only if not registered and not closed previously
-        if (!hasRegistered && (!hasClosed || (currentTime - hasClosed > 86400000))) {
-            const timer = setTimeout(() => {
-                setShowPopup(true);
-                dataLayer.push({
-                    event: 'popupImpression',
-                });
-            }, 10000); // 10 seconds delay
+    //     const currentTime = Date.now();
+    //     // Show popup only if not registered and not closed previously
+    //     if (!hasRegistered && (!hasClosed || (currentTime - hasClosed > 86400000))) {
+    //         const timer = setTimeout(() => {
+    //             setShowPopup(true);
+    //             dataLayer.push({
+    //                 event: 'popupImpression',
+    //             });
+    //         }, 10000); // 10 seconds delay
 
-            return () => clearTimeout(timer); // Clean up the timer
-        }
-    }, []);
+    //         return () => clearTimeout(timer); // Clean up the timer
+    //     }
+    // }, []);
 
     const closePopup = () => {
         setShowPopup(false);
