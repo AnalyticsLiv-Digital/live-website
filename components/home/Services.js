2e318e9baa8362108cefcce43353ab527ddb56e9
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-02-06 12:58:17 +0530

    services changes in home

--- components/home/Services.js
+++ components/home/Services.js
@@ -7,7 +7,7 @@ const Services = () => {
         {
             title: "Data Foundation",
             bgColor: "#E7FBF9",
-            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/data-warehouse%20(1)%201.png",
+            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Data%20Foundation.png",
             items: ["Account Health & Opportunity", " GA4 Implementation & Optimization", "Server Side Tracking", "Cookie Consent Solution"],
             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/GA4_Logo%20(1).png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/GTM_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Firebase_Logo%20(1).png"],
             redirect: "/services/web-app-measurement",
@@ -15,7 +15,7 @@ const Services = () => {
         {
             title: "Data Transformation",
             bgColor: "#FFF4E2",
-            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/dashboard%20(8)%201.png",
+            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Data%20Transformation.png",
             items: ["Cloud Migration & Engineering", "Custom Pipeline Creation", "Custom Dashboarding"],
             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Looker_Studio_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/BQ_Logo.png"],
             redirect: "/services/data-science",
@@ -23,7 +23,7 @@ const Services = () => {
         {
             title: "Data Driven Marketing",
             bgColor: "#C8C4E9",
-            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/data-driven%20(3).png",
+            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Data%20Driven%20Marketing.png",
             items: ["PPC Campaign Management", "Programmatic Advertising (DV360)", "CRO-Consumer Journey, Mapping, Strategy, Desing", "Data Insight"],
             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/DV_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Google_Ads_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Meta_Ads_Logo.png"],
             redirect: "/services/programatic-advertising",
@@ -31,68 +31,193 @@ const Services = () => {
         {
             title: "Product & Solutions",
             bgColor: "#C8C4E9",
-            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/prototype%201.png",
-            items: ["MAdKPI", "Lead Scoring", "Product & Content Recommendation Engine", "RFM Analysis", "MMM Modeling"],
+            image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Product%20and%20Solutions.png",
+            items: ["MAdKPI", "Lead Scoring", "Product and CRE (Content Recommendation Engine)", "RFM Analysis", "MMM Modeling"],
             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/MAD_KPI_Logo%20(2).png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/DV_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Google_Ads_Logo.png"],
             redirect: "/services/customer-segmentation",
         },
     ];
 
     return (
-        <section className="pt-14 lg:mb-12 2xl:mb-5 px-4 md:px-8 lg:px-16 relative">
-            <div className="text-[#100F1B] text-3xl font-bold text-center">Services We Offer</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center max-md:gap-7 pt-12 pb-6 relative">
-                {services.map((service, index) => (
-                    <div
-                        key={index}
-                        className={`flex flex-col group md:hover:scale-105 transition duration-300 gap-5 w-[310px] sm:w-[350px] md:w-[350px] md:h-[280px] lg:w-[450px] xl:w-[500px] 2xl:w-[650px] shadow-customShadow hover:bg-[#08A4F7] hover:text-white px-4 md:px-7 py-4 sm:py-5 rounded-3xl
-                ${index === 1 || index === services.length - 1 ? 'md:mt-12' : ''}`}
-                    >
-                        <div>
-                            <div className="flex justify-start items-center gap-5">
-                                <img src={service.image} alt={service.title} />
-                                <h3 className="text-lg font-bold text-center">{service.title}</h3>
-                            </div>
+      <div className="pt-14 mb-12 xl:mb-20 2xl:mb-12 px-4 md:px-8 lg:px-16 relative">
+        <div className="text-[#100F1B] text-3xl font-bold text-center">
+          Services We Offer
+        </div>
+        <div className="relative">
+        <div className="flex flex-col justify-start md:w-[50%] gap-6 pt-12">
+          <div className="text-lg max-md:text-center md:text-3xl font-bold text-[#373642]">Checkout our Expert Services</div>
+          <div className="text-sm max-md:text-center md:text-base font-normal text-[#373642]">
+            Analytics Liv Digital (“AnalyticsLiv”) is a new age marketing firm
+            focused on delivering data driven customer experiences. Cutting
+            through the noise of digital data fatigue to take cognizance of a
+            brand’s first party data is our prime focus. A website, mobile app,
+            PPC campaigns or landing pages- whatever be your customer
+            touchpoint(s), we will make it count.
+          </div>
+        </div>
+        <img src="https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/industrial-designers-working-together%201.svg" alt="services"
+        className="max-md:hidden md:absolute top-10 right-[5%] 2xl:right-[9%] z-10 max-xl:w-[40%]" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 z-20 max-md:mx-7 justify-items-center 2xl:justify-items-start max-lg:gap-7 pt-12 pb-6 relative">
+          {services.map((service, index) => (
+            <div
+              key={index}
+              className={`flex flex-col group transition duration-300 gap-5 w-[310px] sm:w-[350px] md:w-[350px] 
+                md:h-[280px] lg:w-[205px] xl:w-[280px] 2xl:w-[300px] min-h-[300px] xl:min-h-[326px] shadow-customShadow
+                px-4 xl:px-7 py-4 sm:py-5 rounded-3xl bg-white
+               `}
+            >
+              <div>
+                <div className="flex flex-col justify-center items-center gap-5">
+                  <img src={service.image} alt={service.title} className="w-14" />
+                  <h3 className="text-base xl:text-lg font-bold text-center">
+                    {service.title}
+                  </h3>
+                </div>
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col justify-start items-start gap-3">
+                  <div className="flex flex-col gap-2.5">
+                    {service?.items.map((item, idx) => (
+                      <div
+                        key={idx}
+                        className="flex flex-col"
+                      >
+                        <div className="flex items-center md:items-start gap-1 sm:gap-2 text-[10px] xl:text-sm hover:cursor-default font-normal">
+                          <img
+                            src="https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Check%20Mark.png"
+                            alt="Check Mark blue"
+                            className="pt-0.5"
+                          />
+                          {item}
                         </div>
-                        <div className="flex justify-between items-center">
-                            <div className="flex flex-col justify-start items-start gap-3">
-                                <div className="flex flex-col gap-2.5">
-                                    {service?.items.map((item, idx) => (
-                                        <div key={idx} className="flex flex-col max-w-[150px] md:max-w-[160px] lg:max-w-[200px] xl:max-w-[250px] 2xl:max-w-[450px]">
-                                            <div className="flex items-start gap-1 sm:gap-2 text-[10px] lg:text-xs xl:text-sm hover:cursor-default font-normal">
-                                                <img
-                                                    src="https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Check%20Mark.png"
-                                                    alt="Check Mark blue"
-                                                    className="pt-0.5 group-hover:hidden"
-                                                />
-                                                <img
-                                                    src="https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/checked%20(1).png"
-                                                    alt="Check Mark white"
-                                                    className="pt-0.5 hidden group-hover:flex"
-                                                />
-                                                {item}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                            <div className="flex flex-col justify-center items-center">
-                                <div className="flex lg:flex-wrap justify-start items-center gap-3">
-                                    {service.images.map((img, idx) => (
-                                        <img key={idx} src={img} alt={`service-${index}-${idx}`} className="lg:p-1 rounded-lg mb-2 lg:mb-3 max-md:w-8 max-lg:w-10" />
-                                    ))}
-                                </div>
-                                <a href={service.redirect} target="_blank"><button className="mainbutn-opposite hover:border-white">Read More</button></a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                {/* <div className="flex flex-col justify-center items-center">
+                  <div className="flex lg:flex-wrap justify-start items-center gap-3">
+                    {service.images.map((img, idx) => (
+                      <img
+                        key={idx}
+                        src={img}
+                        alt={`service-${index}-${idx}`}
+                        className="lg:p-1 rounded-lg mb-2 lg:mb-3 max-md:w-8 max-lg:w-10"
+                      />
+                    ))}
+                  </div>
+                  <a href={service.redirect} target="_blank">
+                    <button className="mainbutn-opposite hover:border-white">
+                      Read More
+                    </button>
+                  </a>
+                </div> */}
+              </div>
             </div>
-        </section>
+          ))}
+        </div>
+        </div>
+        </div>
     );
 };
 
 export default Services;
+
+
+// import React from "react";
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+// const Services = () => {
+
+//     const services = [
+//         {
+//             title: "Data Foundation",
+//             bgColor: "#E7FBF9",
+//             image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/data-warehouse%20(1)%201.png",
+//             items: ["Account Health & Opportunity", " GA4 Implementation & Optimization", "Server Side Tracking", "Cookie Consent Solution"],
+//             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/GA4_Logo%20(1).png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/GTM_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Firebase_Logo%20(1).png"],
+//             redirect: "/services/web-app-measurement",
+//         },
+//         {
+//             title: "Data Transformation",
+//             bgColor: "#FFF4E2",
+//             image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/dashboard%20(8)%201.png",
+//             items: ["Cloud Migration & Engineering", "Custom Pipeline Creation", "Custom Dashboarding"],
+//             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Looker_Studio_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/BQ_Logo.png"],
+//             redirect: "/services/data-science",
+//         },
+//         {
+//             title: "Data Driven Marketing",
+//             bgColor: "#C8C4E9",
+//             image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/data-driven%20(3).png",
+//             items: ["PPC Campaign Management", "Programmatic Advertising (DV360)", "CRO-Consumer Journey, Mapping, Strategy, Desing", "Data Insight"],
+//             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/DV_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Google_Ads_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Meta_Ads_Logo.png"],
+//             redirect: "/services/programatic-advertising",
+//         },
+//         {
+//             title: "Product & Solutions",
+//             bgColor: "#C8C4E9",
+//             image: "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/prototype%201.png",
+//             items: ["MAdKPI", "Lead Scoring", "Product & Content Recommendation Engine", "RFM Analysis", "MMM Modeling"],
+//             images: ["https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/MAD_KPI_Logo%20(2).png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/DV_Logo.png", "https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Google_Ads_Logo.png"],
+//             redirect: "/services/customer-segmentation",
+//         },
+//     ];
+
+//     return (
+//         <section className="pt-14 lg:mb-12 2xl:mb-5 px-4 md:px-8 lg:px-16 relative">
+//             <div className="text-[#100F1B] text-3xl font-bold text-center">Services We Offer</div>
+//             <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center max-md:gap-7 pt-12 pb-6 relative">
+//                 {services.map((service, index) => (
+//                     <div
+//                         key={index}
+//                         className={`flex flex-col group md:hover:scale-105 transition duration-300 gap-5 w-[310px] sm:w-[350px] md:w-[350px] md:h-[280px] lg:w-[450px] xl:w-[500px] 2xl:w-[650px] shadow-customShadow hover:bg-[#08A4F7] hover:text-white px-4 md:px-7 py-4 sm:py-5 rounded-3xl
+//                 ${index === 1 || index === services.length - 1 ? 'md:mt-12' : ''}`}
+//                     >
+//                         <div>
+//                             <div className="flex justify-start items-center gap-5">
+//                                 <img src={service.image} alt={service.title} />
+//                                 <h3 className="text-lg font-bold text-center">{service.title}</h3>
+//                             </div>
+//                         </div>
+//                         <div className="flex justify-between items-center">
+//                             <div className="flex flex-col justify-start items-start gap-3">
+//                                 <div className="flex flex-col gap-2.5">
+//                                     {service?.items.map((item, idx) => (
+//                                         <div key={idx} className="flex flex-col max-w-[150px] md:max-w-[160px] lg:max-w-[200px] xl:max-w-[250px] 2xl:max-w-[450px]">
+//                                             <div className="flex items-start gap-1 sm:gap-2 text-[10px] lg:text-xs xl:text-sm hover:cursor-default font-normal">
+//                                                 <img
+//                                                     src="https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Check%20Mark.png"
+//                                                     alt="Check Mark blue"
+//                                                     className="pt-0.5 group-hover:hidden"
+//                                                 />
+//                                                 <img
+//                                                     src="https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/checked%20(1).png"
+//                                                     alt="Check Mark white"
+//                                                     className="pt-0.5 hidden group-hover:flex"
+//                                                 />
+//                                                 {item}
+//                                             </div>
+//                                         </div>
+//                                     ))}
+//                                 </div>
+//                             </div>
+//                             <div className="flex flex-col justify-center items-center">
+//                                 <div className="flex lg:flex-wrap justify-start items-center gap-3">
+//                                     {service.images.map((img, idx) => (
+//                                         <img key={idx} src={img} alt={`service-${index}-${idx}`} className="lg:p-1 rounded-lg mb-2 lg:mb-3 max-md:w-8 max-lg:w-10" />
+//                                     ))}
+//                                 </div>
+//                                 <a href={service.redirect} target="_blank"><button className="mainbutn-opposite hover:border-white">Read More</button></a>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default Services;
 
 
 
