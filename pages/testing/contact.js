0bb114513d38c0c01907b0016fc7bbdd10fd5beb
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-03-17 18:32:52 +0530

    minor changes in contact

--- pages/testing/contact.js
+++ pages/testing/contact.js
@@ -107,7 +107,7 @@ const contact = () => {
             <ScrollProgress />
 
             <section className="">
-                <div className="relative h-[350px] sm:h-[420px] lg:h-[460px] 2xl:h-[540px] bg-header-linear sm:px-[10%] 2xl:px-[5%] pt-24">
+                <div className="relative h-[370px] sm:h-[420px] lg:h-[460px] 2xl:h-[540px] bg-header-linear sm:px-[10%] 2xl:px-[5%] pt-[90px] sm:pt-24">
                     <div className="absolute top-0 left-0 sm:px-[5%] lg:px-[10%] 2xl:px-[5%] w-full h-[350px] md:h-[400px]">
                         <img
                             src="https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Contact_Team_Image.svg"
@@ -115,13 +115,13 @@ const contact = () => {
                             className="mx-auto mt-14 object-top max-sm:hidden"
                         />
                         <img
-                            src="https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Contact_Team_Image%20(2).svg"
+                            src="https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Contact_Team_Mobile.svg"
                             alt="AnalyticsLiv Contact"
-                            className="mx-auto w-full px-3 mt-14 object-top sm:hidden"
+                            className="mx-auto w-full px-3 mt-10 object-top sm:hidden"
                         />
                     </div>
                     <div className="relative z-10 flex flex-col items-center sm:pt-12">
-                        <h1 className="text-base sm:text-[35px] 2xl:text-[40px] font-bold text-white">Contact our Support Team</h1>
+                        <h1 className="text-lg sm:text-[35px] 2xl:text-[40px] font-bold text-white">Contact our Support Team</h1>
                         <p className="text-[8px] sm:text-sm 2xl:text-base font-normal text-white pt-2 sm:pt-4 md:pt-8">Questions, bug reports, feedback — we’re here for it all.</p>
                     </div>
                 </div>
@@ -135,7 +135,7 @@ const contact = () => {
                                     <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/clutch_review_4.9.svg' alt='clutch_review' className='pt-10 sm:pt-10 lg:pt-14 max-md:w-[100px] w-[125px] lg:w-[180px]' />
                                     <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Trustpilot_mobile.svg' alt='Trustpilot review' className='pt-10 sm:pt-10 lg:pt-14 max-md:w-[100px] lg:w-[200px]' />
                                     <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Google_Review.svg' alt='Google_Review' className='pt-10 lg:pt-14 max-md:w-[100px] max-lg:w-[125px]' />
-                                    <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Ambitiuon_Box_Review.svg' alt='Ambitiuon_Box_Review' className='pt-10 lg:pt-14 max-md:w-[200px] max-lg::w-[125px]' />
+                                    <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/Ambitiuon_Box_Review.svg' alt='Ambitiuon_Box_Review' className='pt-[48px] lg:pt-14 max-md:w-[200px] max-lg::w-[125px]' />
                                 </div>
                                 <div className='w-full md:pt-10 lg:pt-16'>
                                     <div className='text-left'>
@@ -221,7 +221,7 @@ const contact = () => {
                             {!formSubmit && <form className="md:w-[45%] flex flex-col justify-between gap-4" onSubmit={handleSubmit}>
                                 <div className='w-full'>
                                     <div className='text-sm 2xl:text-lg font-semibold text-[#14183E] pb-1 2xl:pb-1.5'>Full Name*</div>
-                                    <input type="text" placeholder="Enter Your Full Name" className="px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
+                                    <input type="text" placeholder="Enter Your Full Name" className="text-sm 2xl:text-base px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
                                         id="firstname" name="firstName" value={formValues?.firstName} onChange={handleChange} />
                                     <p className="text-red-600 text-sm">{formErrors?.firstName}</p>
                                 </div>
@@ -229,7 +229,7 @@ const contact = () => {
                                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
                                     <div className='w-full'>
                                         <div className='text-sm 2xl:text-lg font-semibold text-[#14183E] pb-1 lg:pb-[5px] lg:pt-1 2xl:pt-0 2xl:pb-1.5'>Email*</div>
-                                        <input type="email" placeholder="Enter Your Email" className="px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
+                                        <input type="email" placeholder="Enter Your Email" className="text-sm 2xl:text-base px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
                                             name="email" value={formValues?.email} onChange={handleChange} />
                                         <p className="text-red-600 text-sm">{formErrors?.email}</p>
 
@@ -268,7 +268,7 @@ const contact = () => {
 
                                 <div>
                                     <div className='text-sm 2xl:text-lg font-semibold text-[#14183E] pb-1 2xl:pb-1.5'>Company Name or Website</div>
-                                    <input type="text" placeholder="Enter Company Name or Website" className="px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
+                                    <input type="text" placeholder="Enter Company Name or Website" className="text-sm 2xl:text-base px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
                                         name="company" value={formValues?.company} onChange={handleChange} />
                                     <p className="text-red-600 text-sm">{formErrors?.company}</p>
 
@@ -276,7 +276,7 @@ const contact = () => {
 
                                 <div>
                                     <div className='text-sm 2xl:text-lg font-semibold text-[#14183E] pb-1 2xl:pb-1.5'>Contact For*</div>
-                                    <select className="px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
+                                    <select className="text-sm 2xl:text-base px-5 py-2 2xl:py-3 border-[0.5px] border-[#08A4F7] rounded-md w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
                                         id="purpose" name="purpose" value={formValues?.purpose} onChange={handleChange}>
                                         <option>Contact For</option>
                                         <option>Web Analytics Service</option>
@@ -293,12 +293,12 @@ const contact = () => {
 
                                 <div className=''>
                                     <div className='text-sm 2xl:text-lg font-semibold text-[#14183E] pb-1 2xl:pb-1.5'>Requirenments*</div>
-                                    <textarea placeholder="Write Message..." rows={2} className="px-5 py-2 border-[0.5px] border-[#08A4F7] rounded-md md:col-span-2 max-h-28 overflow-auto w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
+                                    <textarea placeholder="Write Message..." rows={2} className="text-sm 2xl:text-base px-5 py-2 border-[0.5px] border-[#08A4F7] rounded-md md:col-span-2 max-h-28 overflow-auto w-full focus:outline-none placeholder-[#747582] placeholder-opacity-75"
                                         id="requirements" name="requirements" value={formValues?.requirements} onChange={handleChange}></textarea>
 
                                     <p className="text-red-600 text-sm">{formErrors?.requirements}</p>
 
-                                    <div className='text-[#747582] text-[10px] 2xl:text-xs'>By submitting this form, you agreed to our <a href='/privacy-policy' target='_blank'>privacy policy</a>. We promise we don’t spam.</div>
+                                    <div className='text-[#747582] text-[10px] 2xl:text-xs'>By submitting this form, you agreed to our <a href='/privacy-policy' target='_blank' className='underline'>privacy policy</a>. We promise we don’t spam.</div>
                                 </div>
                                 {showWaiting &&
                                     <button type="submit" style={{ cursor: 'not-allowed' }} disabled className="group mainbutn cursor-not-allowed flex items-center justify-center gap-2">
@@ -338,18 +338,18 @@ const contact = () => {
 
                     </div>
 
-                    <div className="absolute h-[375px] md:h-[460px] 2xl:h-[540px] bottom-[-250px] md:bottom-[-300px] 2xl:bottom-[-370px] w-full z-10 bg-gradient-to-b from-[#003366] to-[#00509E] text-white pt-40 pb-10 flex justify-center items-end">
+                    <div className="absolute h-[375px] sm:h-[375px] md:h-[460px] 2xl:h-[540px] bottom-[-260px] sm:bottom-[-250px] md:bottom-[-300px] 2xl:bottom-[-370px] w-full z-10 bg-gradient-to-b from-[#003366] to-[#00509E] text-white pt-48 sm:pt-40 pb-10 flex justify-center items-end">
                         <div className="h-full w-[95%] lg:w-[1000px] 2xl:w-[1240px] grid grid-cols-3 place-items-start justify-items-center text-center gap-3 sm:gap-10 md:pt-20 2xl:pt-28">
                             <div className='flex flex-col items-center lg:w-[70%]'>
-                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_1.svg' alt='AnalyticsLiv Adress' className='pb-6' />
+                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_1.svg' alt='AnalyticsLiv Adress' className='pb-4 sm:pb-6 max-sm:w-[40px]' />
                                 <p className='pb-1 text-[10px] sm:text-sm 2xl:text-lg font-medium'>503, 31Five, Corporate Road, Prahaladnagar, S.G. Highway, Ahmedabad 380015, Gujarat, India</p>
                             </div>
                             <div className='flex flex-col items-center lg:w-[70%]'>
-                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_2.svg' alt='AnalyticsLiv Adress' className='pb-6' />
+                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_2.svg' alt='AnalyticsLiv Adress' className='pb-4 sm:pb-6 max-sm:w-[40px]' />
                                 <p className='pb-1 text-[10px] sm:text-sm 2xl:text-lg font-medium'>3104 E Camelback Rd ,Suite #7406, Phoenix, AZ 85016, USA</p>
                             </div>
                             <div className='flex flex-col items-center lg:w-[70%]'>
-                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_3.svg' alt='AnalyticsLiv Adress' className='pb-6' />
+                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Images_and_Icons/office_3.svg' alt='AnalyticsLiv Adress' className='pb-4 sm:pb-6 max-sm:w-[40px]' />
                                 <p className='pb-1 text-[10px] sm:text-sm 2xl:text-lg font-medium'>Via Natale Prampolini, 15 - 00132, Roma, Italy</p>
                             </div>
                         </div>
@@ -357,9 +357,9 @@ const contact = () => {
                 </div>
 
                 <div className='mt-[300px] md:mt-[350px] 2xl:mt-[420px] px-[5%] pb-14'>
-                    <div className='bg-header-linear p-[2.5%]'>
+                    <div className='bg-header-linear p-[5%] rounded-[5px] sm:p-[2.5%]'>
                         <div className='w-full h-[200px]'>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.660556214622!2d72.4981082753132!3d22.9995064791899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x613f5a1e7cb48aa3%3A0xaef382143b118f7!2sAnalyticsLiv!5e0!3m2!1sen!2sin!4v1704214142520!5m2!1sen!2sin" className='w-full h-full z-100'></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.660556214622!2d72.4981082753132!3d22.9995064791899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x613f5a1e7cb48aa3%3A0xaef382143b118f7!2sAnalyticsLiv!5e0!3m2!1sen!2sin!4v1704214142520!5m2!1sen!2sin" className='w-full h-full z-100 rounded-[5px]'></iframe>
                         </div>
                     </div>
                 </div>
