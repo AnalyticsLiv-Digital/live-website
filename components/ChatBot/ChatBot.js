f465e72439c97ff9946bfbf44a9a126ef711f3bf
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-04-09 17:24:55 +0530

    Chatbot changes done

--- components/ChatBot/ChatBot.js
+++ components/ChatBot/ChatBot.js
@@ -25,8 +25,8 @@ export default function App() {
     <div className="">
       {showBot && (
         <div
-          className="max-md:my-8 md:m-8 flex flex-col md:w-[370px] max-h-[380px] 2xl:max-h-[450px] min-h-[250px] 2xl:min-h-[300px] fixed right-10 bottom-[55px] z-[9999] bg-white shadow-lg rounded-md overflow-y-hidden custom-scrollbar"
-          // style={{ maxHeight: "400px", minHeight: "300px" }}
+          className="max-md:my-8 md:m-8 flex flex-col md:w-[370px] max-h-[380px] 2xl:max-h-[450px] min-h-[250px] 2xl:min-h-[300px] fixed right-10 bottom-[55px] 2xl:bottom-[72px] z-[9999] bg-white shadow-lg rounded-md overflow-y-hidden custom-scrollbar"
+        // style={{ maxHeight: "400px", minHeight: "300px" }}
         >
           <Chatbot
             config={config}
@@ -35,18 +35,19 @@ export default function App() {
             // messageHistory={loadMessages()}
             // saveMessages={saveMessages}
             runInitialMessagesWithHistory={true}
-            // disableScrollToBottom={true}
+          // disableScrollToBottom={true}
           />
         </div>
       )}
       <button
-        className="app-chatbot-button"
+        className="app-chatbot-button right-5 md:right-10 h-16 2xl:h-20 w-16 2xl:w-20"
         onClick={() => setShowBot((prev) => !prev)}
       >
-        <div>Albot</div>
+        <img src="/ChatBot_1.gif" />
+        {/* <div>Albot</div>
         <svg viewBox="0 0 640 512" className="app-chatbot-button-icon">
           <path d="M192,408h64V360H192ZM576,192H544a95.99975,95.99975,0,0,0-96-96H344V24a24,24,0,0,0-48,0V96H192a95.99975,95.99975,0,0,0-96,96H64a47.99987,47.99987,0,0,0-48,48V368a47.99987,47.99987,0,0,0,48,48H96a95.99975,95.99975,0,0,0,96,96H448a95.99975,95.99975,0,0,0,96-96h32a47.99987,47.99987,0,0,0,48-48V240A47.99987,47.99987,0,0,0,576,192ZM96,368H64V240H96Zm400,48a48.14061,48.14061,0,0,1-48,48H192a48.14061,48.14061,0,0,1-48-48V192a47.99987,47.99987,0,0,1,48-48H448a47.99987,47.99987,0,0,1,48,48Zm80-48H544V240h32ZM240,208a48,48,0,1,0,48,48A47.99612,47.99612,0,0,0,240,208Zm160,0a48,48,0,1,0,48,48A47.99612,47.99612,0,0,0,400,208ZM384,408h64V360H384Zm-96,0h64V360H288Z"></path>
-        </svg>
+        </svg> */}
       </button>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,9 @@ import FAQ from '../components/home/FAQ'
 import MetaSchemaOg from '../components/MetaSchemaOg'
 import Head from 'next/head';
 import { schemaHome } from '../utils/schema';
-import ScrollProgress from '../components/ScrollProgress';
+// import YtPlaylist from '../components/YtPlaylist';
 
-const page = () => {
+const page = ({ ytPlaylistdata }) => {
   const stuctureData = { "@context": "https://schema.org", "@graph": [{ "@type": "WebPage", "@id": "https://analyticsliv.com/", "url": "https://analyticsliv.com/", "name": "Leading Web and App Analytics Agency in India - AnalyticsLiv", "isPartOf": { "@id": "https://analyticsliv.com/#website" }, "primaryImageOfPage": { "@id": "https://analyticsliv.com/#primaryimage" }, "image": { "@id": "https://analyticsliv.com/#primaryimage" }, "thumbnailUrl": "https://storage.googleapis.com/website-bucket-uploads/static/logo.png", "datePublished": "2023-01-11T18:27:34+00:00", "dateModified": "2023-04-07T20:48:38+00:00", "description": "AnalyticsLiv Digital is one of the leading Web and App analytics agency. We help our customers embrace Google Products to improve their customer experiences.", "breadcrumb": { "@id": "https://analyticsliv.com/#breadcrumb" }, "inLanguage": "en-US", "potentialAction": [{ "@type": "ReadAction", "target": ["https://analyticsliv.com/"] }] }, { "@type": "ImageObject", "inLanguage": "en-US", "@id": "https://analyticsliv.com/#primaryimage", "url": "https://storage.googleapis.com/website-bucket-uploads/static/logo.png", "contentUrl": "https://storage.googleapis.com/website-bucket-uploads/static/logo.png", "width": 1200, "height": 628 }, { "@type": "BreadcrumbList", "@id": "https://analyticsliv.com/#breadcrumb", "itemListElement": [{ "@type": "ListItem", "position": 1, "name": "Home" }] }, { "@type": "WebSite", "@id": "https://analyticsliv.com/#website", "url": "https://analyticsliv.com/", "name": "Analyticsliv", "description": "", "potentialAction": [{ "@type": "SearchAction", "target": { "@type": "EntryPoint", "urlTemplate": "https://analyticsliv.com/blogs/search?s={search_term_string}" }, "query-input": "required name=search_term_string" }], "inLanguage": "en-US" }, { "@type": "Organization", "@id": "https://analyticsliv.com/#organization", "name": "Analyticsliv", "url": "https://analyticsliv.com/", "logo": { "@type": "ImageObject", "inLanguage": "en-US", "@id": "https://analyticsliv.com/#/schema/logo/image/", "url": "https://storage.googleapis.com/website-bucket-uploads/static/logo.png", "contentUrl": "https://storage.googleapis.com/website-bucket-uploads/static/logo.png", "width": 512, "height": 114, "caption": "Analyticsliv" }, "image": { "@id": "https://analyticsliv.com/#/schema/logo/image/" }, "sameAs": ["https://m.facebook.com/100070503960704/", "https://in.linkedin.com/company/analytics-liv-digital", "https://www.youtube.com/channel/UCSU9utLB2PDe4VcXiI5kMFw", "https://www.instagram.com/analyticsliv_digital"] }] };
 
   return (
@@ -32,13 +32,19 @@ const page = () => {
         description="Data Influences every touchpoint. As GMP partner we help brands from acquisition to conversion and better retention using personalized data backed solutions"
         twitterTitle="AnalyticsLiv | Google Marketing Platform (GMP) Certified Partner"
         twitterDescription="Data Influences every touchpoint. As GMP partner we help brands from acquisition to conversion and better retention using personalized data backed solutions"
+        extraHead={
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&display=swap"
+          />
+        }
       />
       {/* <StructuredData data={stuctureData} /> */}
-      <div className="font-sans">
-      <ScrollProgress color="red" showSpinner={false} />
+      <div className="">
         <Landing />
         <Stats />
         <Services />
+        {/* <YtPlaylist ytPlaylistdata={ytPlaylistdata} name={'LookerBi'} /> */}
         <TrustedByWorldwide />
         <WhyChooseUs />
         <Collaborate />
@@ -53,6 +59,18 @@ const page = () => {
     </>
   )
 }
+// export async function getServerSideProps(context) {
+//   // Fetch data from external API
+//   console.log("-------------------HOLA--------------------------")
+//   const res = await fetch(`${process.env.domain}/api/allytplaylists`)
+//   const ytPlaylistdata = await res.json()
+//   console.log("server ytPlaylistdata -----", ytPlaylistdata)
+
+
+//   //console.log(ytPlaylistdata);
+//   // Pass data to the page via props
+//   return { props: { ytPlaylistdata } }
+// }
 
 export default page
 
@@ -68,7 +86,6 @@ export default page
 // import JoinUs from '../components/home/JoinUs'
 // import ServicesOffer from '../components/home/ServicesOffer'
 // import Embed from '../components/home/Embed'
-// import ScrollProgress from '../components/ScrollProgress';
 // import StructuredData from '../components/StructuredData';
 // import React from 'react'
 // import Head from 'next/head'
