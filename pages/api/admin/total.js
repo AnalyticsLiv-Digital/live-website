389e0d4a97cd49ee1de2b9a87cca4ceb7cf2b2f8
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-04-10 17:36:52 +0530

    Created Leads sectrion in admin which will store all leads of every forms on website

--- pages/api/admin/total.js
+++ pages/api/admin/total.js
@@ -8,28 +8,35 @@ import Gtmcontact from "../../../models/Gtmcontact";
 import Jobapplication from "../../../models/Jobapplication";
 import Ecommercecontact from "../../../models/Ecommercecontact";
 import connectDb from "../../../middleware/mongoose";
-
+import ChatBotLead from "../../../models/ChatBotLead";
+import PopupContact from "../../../models/PopupContact";
+import YtPlaylist from "../../../models/YtPlaylist";
 
 const handler = async (req, res) => {
 
-            let blogs = await Blog.find().count();
-            let casestudies = await Casestudy.find().count();
-            let jobs = await Job.find().count();
-            let jobapplications = await Jobapplication.find().count();
+    let blogs = await Blog.find().count();
+    let casestudies = await Casestudy.find().count();
+    let jobs = await Job.find().count();
+    let jobapplications = await Jobapplication.find().count();
 
-            let contacts = await Contact.find().count();
-            let dv360contacts = await Dv360contact.find().count();
-            let ga4contacts = await Ga4contact.find().count();
-            let gtmcontacts = await Gtmcontact.find().count();
-            let ecommercecontacts = await Ecommercecontact.find().count();
+    let contacts = await Contact.find().count();
+    let dv360contacts = await Dv360contact.find().count();
+    let ga4contacts = await Ga4contact.find().count();
+    let gtmcontacts = await Gtmcontact.find().count();
+    let ecommercecontacts = await Ecommercecontact.find().count();
+    let ChatBotLeads = await ChatBotLead.find().count();
+    let PopupContacts = await PopupContact.find().count();
+    let ytplaylist = await YtPlaylist.find().count();
 
-            let leads = contacts+dv360contacts+ga4contacts+gtmcontacts+ecommercecontacts;
+    let leads = contacts + dv360contacts + ga4contacts + gtmcontacts + ecommercecontacts + ChatBotLeads + PopupContacts + ytplaylist;
 
 
-            res.status(200).json({ blog:blogs, casestudy:casestudies,job:jobs,leads:leads, jobapplications:jobapplications  });
+    res.status(200).json({
+        blog: blogs, casestudy: casestudies, job: jobs, leads: leads, jobapplications: jobapplications,
+        contacts: contacts, dv360contacts: dv360contacts, ga4contacts: ga4contacts, gtmcontacts: gtmcontacts, ecommercecontacts: ecommercecontacts,
+        ChatBotLeads: ChatBotLeads, PopupContacts:PopupContacts, ytplaylist:ytplaylist
+    });
 
-        
-    
 }
 
 export default connectDb(handler);
