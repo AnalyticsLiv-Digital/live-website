e77d9698b0fb9286381efd89a065ffdcaadd5c24
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-04-28 13:06:31 +0530

    Created a component for brands trusted by worldwide will be used in all pages new design

--- pages/services/custom-pipeline.js
+++ pages/services/custom-pipeline.js
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import MetaSchemaOg from '../../components/MetaSchemaOg'
 import { InlineWidget } from 'react-calendly';
+import Brands from '../../components/brands';
 
 const customPipeline = () => {
 
@@ -255,24 +256,7 @@ const customPipeline = () => {
                 </section>
 
                 <section className='px-4 md:px-[5%] py-12'>
-                    <div className='border border-[#D0D0D0] flex max-md:flex-col justify-evenly max-md:gap-8 md:gap-2 xl:gap-1 2xl:gap-5 items-center max-md:py-6 p-3 md:p-5 xl:p-7 2xl:p-10'>
-                        <div className='text-[#0E1947] text-lg max-sm:text-center md:text-sm xl:text-lg font-bold'>Trusted by over 200+ businesses worldwide!</div>
-                        <div className='grid grid-cols-5 max-md:grid-cols-2 place-items-center gap-6'>
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Shoebacca%20logo.png' alt='shoebecca' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Washington_examinor_logo.png' alt='Washington_examinor_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Anantara_logo_New.png' alt='Anantara_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Auxi_logo.coloured.png' alt='Auxi_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Algovation_logo.png' alt='Algovation_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/shiprocket_logo.png' alt='shiprocket_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/we_sort_it_logo.png' alt='we_sort_it_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/Mex_insurance_logo.png' alt='Mex_insurance_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/issta_Logo.png' alt='issta_Logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            <div className='flex items-center gap-5 2xl:gap-10'>
-                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/IRIS_LOGO.png' alt='IRIS_LOGO' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                                <img src='https://storage.googleapis.com/website-bucket-uploads/home_page/Homepage_Img/league_logo_svg%201.png' alt='league_logo' className='max-md:max-h-10 max-xl:max-h-8 max-2xl:max-h-10' />
-                            </div>
-                        </div>
-                    </div>
+                    <Brands />
                 </section>
 
                 <section className='px-4 lg:px-[5%] md:pt-6 flex max-md:flex-col justify-center items-center gap-8 md:gap-5 lg:gap-24 2xl:gap-10'>
