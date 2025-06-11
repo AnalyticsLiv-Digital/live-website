8aefc972f4ea9e7cfc8cf84ce7a73a73a4d99418
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-06-11 13:15:40 +0530

    removed extras div

--- components/dv360/Globalreach.js
+++ components/dv360/Globalreach.js
@@ -4,77 +4,73 @@ import Image from 'next/image'
 const Globalreach = () => {
     return (
         <>
-            <section className='globalreach bg-dvbackgrnd'>
-                <div className='px-4 md:px-20 pt-8 md:pt-11 pb-8 md:pb-16'>
-                    <div className='innerglobl'>
-                        <div className='grid lg:grid-cols-2 gap-7 pt-10'>
-                            <div className='flex max-[1023px]:justify-center'>
-                                <div className='relative flex'>
-                                    <Image
-                                        src="/global.png"
-                                        width={500}
-                                        height={500}
-                                        alt="Picture of client"
-                                        className=''
-                                    />
-                                    <Image
-                                        src="https://storage.googleapis.com/website-bucket-uploads/static/public/Boy.png"
-                                        width={500}
-                                        height={500}
-                                        alt="Picture of client"
-                                        className='absolute d bottom-[44px] -left-[132px] max-[425px]:bottom-[36px] max-[425px]:-left-[100px]'
-                                    />
-                                </div>
+            <section className=''>
+                <div className='grid lg:grid-cols-2 gap-7 pt-10'>
+                    <div className='flex max-[1023px]:justify-center'>
+                        <div className='relative flex'>
+                            <Image
+                                src="/global.png"
+                                width={500}
+                                height={500}
+                                alt="Picture of client"
+                                className=''
+                            />
+                            <Image
+                                src="https://storage.googleapis.com/website-bucket-uploads/static/public/Boy.png"
+                                width={500}
+                                height={500}
+                                alt="Picture of client"
+                                className='absolute d bottom-[44px] -left-[132px] max-[425px]:bottom-[36px] max-[425px]:-left-[100px]'
+                            />
+                        </div>
+                    </div>
+                    <div className='global-points grid gap-5'>
+                        <div className="point1 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
+                            <Image
+                                src="/point1.svg"
+                                width={40}
+                                height={40}
+                                alt="Picture of client"
+                            />
+                            <div>
+                                <h4 className='text-[#000] text-sm font-semibold leading-normal'>Exchanges</h4>
+                                <p className='text-[#000] text-xs font-normal leading-normal'>100+ Exchanges and Sub Exchanges</p>
                             </div>
-                            <div className='global-points grid gap-5'>
-                                <div className="point1 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
-                                    <Image
-                                        src="/point1.svg"
-                                        width={40}
-                                        height={40}
-                                        alt="Picture of client"
-                                    />
-                                    <div>
-                                        <h4 className='text-[#000] text-sm font-semibold leading-normal'>Exchanges</h4>
-                                        <p className='text-[#000] text-xs font-normal leading-normal'>100+ Exchanges and Sub Exchanges</p>
-                                    </div>
-                                </div>
-                                <div className="point2 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
-                                    <Image
-                                        src="/point2.svg"
-                                        width={40}
-                                        height={40}
-                                        alt="Picture of client"
-                                    />
-                                    <div>
-                                        <h4 className='text-[#000] text-sm font-semibold leading-normal'>Programmatic Advertising Platforms</h4>
-                                        <p className='text-[#000] text-xs font-normal leading-normal'>Buy Display, Video, Audio, Native, Connected TV Ads</p>
-                                    </div>
-                                </div>
-                                <div className="point3 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
-                                    <Image
-                                        src="/point3.svg"
-                                        width={40}
-                                        height={40}
-                                        alt="Picture of client"
-                                    />
-                                    <div>
-                                        <h4 className='text-[#000] text-sm font-semibold leading-normal'>Audience Segmentation</h4>
-                                        <p className='text-[#000] text-xs font-normal leading-normal'>3rd Party Audience Availability</p>
-                                    </div>
-                                </div>
-                                <div className="point4 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
-                                    <Image
-                                        src="/point4.svg"
-                                        width={40}
-                                        height={40}
-                                        alt="Picture of client"
-                                    />
-                                    <div>
-                                        <h4 className='text-[#000] text-sm font-semibold leading-normal'>Ads Frequency</h4>
-                                        <p className='text-[#000] text-xs font-normal leading-normal'>Consolidated Frequency Management</p>
-                                    </div>
-                                </div>
+                        </div>
+                        <div className="point2 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
+                            <Image
+                                src="/point2.svg"
+                                width={40}
+                                height={40}
+                                alt="Picture of client"
+                            />
+                            <div>
+                                <h4 className='text-[#000] text-sm font-semibold leading-normal'>Programmatic Advertising Platforms</h4>
+                                <p className='text-[#000] text-xs font-normal leading-normal'>Buy Display, Video, Audio, Native, Connected TV Ads</p>
+                            </div>
+                        </div>
+                        <div className="point3 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
+                            <Image
+                                src="/point3.svg"
+                                width={40}
+                                height={40}
+                                alt="Picture of client"
+                            />
+                            <div>
+                                <h4 className='text-[#000] text-sm font-semibold leading-normal'>Audience Segmentation</h4>
+                                <p className='text-[#000] text-xs font-normal leading-normal'>3rd Party Audience Availability</p>
+                            </div>
+                        </div>
+                        <div className="point4 bg-[#fff] flex gap-5 rounded-[10px] border border-solid border-[#E2E2E2] hover:shadow-testishadow p-5">
+                            <Image
+                                src="/point4.svg"
+                                width={40}
+                                height={40}
+                                alt="Picture of client"
+                            />
+                            <div>
+                                <h4 className='text-[#000] text-sm font-semibold leading-normal'>Ads Frequency</h4>
+                                <p className='text-[#000] text-xs font-normal leading-normal'>Consolidated Frequency Management</p>
                             </div>
                         </div>
                     </div>
