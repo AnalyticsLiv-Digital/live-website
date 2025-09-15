e58ad476d619a182583188c4af887e34911f7cb6
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-09-15 14:04:18 +0530

    career page prev version restore to track the server side issue

--- models/Jobapplication.js
+++ models/Jobapplication.js
@@ -1,17 +1,35 @@
 const mongoose = require('mongoose');
 
 const JobapplicationSchema = new mongoose.Schema({
-    post: { type: String },
-    fullName: { type: String, required: true },
+    post: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
-    contact: { type: String },
+    contact: { type: String, required: true },
     postName: { type: String, required: true },
     linkedin: { type: String },
-    experience: { type: String },
+    experience: { type: String, required: true },
     resume: { type: String, required: true },
     noticePeriod: { type: String },
-    coverLetter: { type: String },
     timestamp: { type: Date, default: Date.now }
 });
 mongoose.models = {};
 export default mongoose.model("Jobapplication", JobapplicationSchema);
+
+// const mongoose = require('mongoose');
+
+// const JobapplicationSchema = new mongoose.Schema({
+//     post: { type: String },
+//     fullName: { type: String, required: true },
+//     email: { type: String, required: true },
+//     contact: { type: String },
+//     postName: { type: String, required: true },
+//     linkedin: { type: String },
+//     experience: { type: String },
+//     resume: { type: String, required: true },
+//     noticePeriod: { type: String },
+//     coverLetter: { type: String },
+//     timestamp: { type: Date, default: Date.now }
+// });
+// mongoose.models = {};
+// export default mongoose.model("Jobapplication", JobapplicationSchema);
