064e07ca7e2d2c9ebf4416d3a6f7095141e65f06
Pruthviraj <pruthvirajsinh.rathod@analyticsliv.com>
2025-12-26 17:54:13 +0530

    minor changes

--- pages/services/digital-maturity-assessment.js
+++ pages/services/digital-maturity-assessment.js
@@ -15,6 +15,7 @@ import PrintableContent from '../../components/digitalMaturityAssessment/Printab
 const STORAGE_KEY = "al_maturity_v2";
 const SNAP_KEY = "s";
 
+
 export default function Home() {
     const [mode, setMode] = useState('general');
     const [depth, setDepth] = useState('quick');
